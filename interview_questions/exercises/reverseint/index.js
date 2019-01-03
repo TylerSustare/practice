4934e185ff20c5b43e8c716031f8732cafece7b0
G. Tyler Sustare <tylersustare@gmail.com>
2019-01-03 22:14:00 +0000

    finish reverse int

--- interview_questions/exercises/reverseint/index.js
+++ interview_questions/exercises/reverseint/index.js
@@ -8,6 +8,20 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+/* my first solution O(n) */
+function reverseInt(n) {
+    if (n >= 0) {
+        return Number(n.toString().split('').reverse().join(''));
+    }
+    return Number('-' + n.toString().replace('-', '').split('').reverse().join(''));
+}
+
 
 module.exports = reverseInt;
+
+/* first solution given O(n)
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('');
+    return Math.sign(n) * parseInt(reversed);
+}
+*/

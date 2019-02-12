0180e32203b10a8a7f2d01f9478fce643f7b9c36
G. Tyler Sustare <tylersustare@gmail.com>
2019-02-12 22:37:46 +0000

    finish chunk

--- interview_questions/exercises/chunk/index.js
+++ interview_questions/exercises/chunk/index.js
@@ -8,6 +8,46 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// my solution O(n)
+function chunk(array, size) {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+}
 
 module.exports = chunk;
+
+/*
+// their first solution
+function chunk(array, size) {
+    const chunked = [];
+
+    for (let element of array) {
+        const last = chunked[chunked.length - 1];
+
+        if (!last || last.length === size) {
+            chunked.push([element]) // new chunk
+        }
+        else {
+            last.push(element); // add element to chunk
+        }
+    }
+    return chunked;
+}
+*/
+
+/*
+// their second solution (just used a while loop instead)
+function chunk(array, size) {
+    const chunked = [];
+    let i = 0;
+
+    while (i < array.length) {
+        chunked.push(array.slice(i, i + size));
+        i += size;
+    }
+    return chunked;
+}
+*/

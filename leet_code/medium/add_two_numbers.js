1df47f4b4e0370ee3f32b0bb1b3ab75b42e8c7e8
Tyler Sustare <tyler.sustare@gmail.com>
2019-04-09 19:29:06 +0000

    add two numbers

--- leet_code/medium/add_two_numbers.js
+++ leet_code/medium/add_two_numbers.js
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/add-two-numbers/
 /*
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -10,3 +12,43 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  let head = new ListNode(); // dummy head
+  let curr = head;
+  let sum = 0;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null || sum > 0) { // sum > 0 means we have a carry
+    if (l1 !== null) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    if (sum >= 10) {
+      carry = 1;
+      sum -= 10;
+    }
+    curr.next = new ListNode(sum);
+    curr = curr.next;
+
+    sum = carry;
+    carry = 0;
+  }
+  return head.next;
+};
